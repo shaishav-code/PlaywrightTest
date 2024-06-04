047a535ead9ef7df9d3afd94b89ff811007bc81f
shaishav-code <shd.narola@gmail.com>
2024-06-04 15:04:36 +0530

    updated  code

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Rahul Shetty test cases', async({ page }) => {
+test('Practice Page', async({ page }) => {
 
 
     await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+
+    await page.locator('input[value="radio1"]').isVisible;
+
+    await page.locator('input[value="radio1"]').click;
+
+    await page.locator('input[id="autocomplete"]').isVisible;
+    await page.locator('input[id="autocomplete"]').fill('India');
+
+    await page.locator
 })
