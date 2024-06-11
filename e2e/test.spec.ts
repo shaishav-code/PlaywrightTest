bba0d7b599c640a3a75267a486b83ad9116be69e
shaishav-code <shd.narola@gmail.com>
2024-06-11 16:16:35 +0530

    updated  code

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -5,12 +5,27 @@ test('Practice Page', async({ page }) => {
 
     await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
 
-    await page.locator('input[value="radio1"]').isVisible;
+    await page.locator('input[value="radio1"]').isVisible();
 
-    await page.locator('input[value="radio1"]').click;
+    await page.locator('input[value="radio1"]').click();
 
-    await page.locator('input[id="autocomplete"]').isVisible;
+    // await page.pause();
+    await page.locator('input[id="autocomplete"]').isVisible();
     await page.locator('input[id="autocomplete"]').fill('India');
+    // await page.pause();
+    await page.keyboard.press('ArrowDown');
+    // await page.locator('input[id="autocomplete"]').isVisible();
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('Enter');
 
-    await page.locator
+    // await page.pause();
+    await page.locator('select[id="dropdown-class-example"]').click();
+    await page.locator('select[id="dropdown-class-example"]').selectOption({value :"option1"});
+
+    // await page.pause();
+    await page.locator('input[id="checkBoxOption1"]').click();
+
+    // await page.pause();
+
+
 })
